c20b5e1579e100760841caa9e0d03db7942f3ba2
lethaboAphane <lethabo_aphane@icloud.com>
2023-08-25 04:57:54 -0700

    printf

--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 #define BUFF_SIZE 1024
 
 /* FLAGS */
+
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
@@ -16,6 +17,7 @@
 #define F_SPACE 16
 
 /* SIZES */
+
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -27,6 +29,7 @@
  */
 
 struct fmt
+
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
